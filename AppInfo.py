4d42d07c45a00832722335457633ac7915848a6d
sidshukla-github <siddharth.shukla@gmail.com>
2020-07-19 23:45:03 +1000

    cloud build

--- AppInfo.py
+++ AppInfo.py
@@ -1,8 +1,7 @@
+import requests
+import json
 import flask 
 from flask import request, jsonify
-
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
 
 appdetail = {
     "myapplication": [
@@ -14,16 +13,48 @@ appdetail = {
 ]
 }
 
+urlid = 'https://api.github.com/repos/sidshukla-github/ANZAPITest/commits'
+
+headers = {
+    "Authorization" : "Token dc81e6fca81357528b4fc1e4f6c3b32473869021",
+      "Content-Type": "application/json",
+      "User-Agent" : "sidshukla-github"  
+    }
+
+def invokegithubapi():
+    
+    response = requests.request("GET", urlid, headers=headers)
+
+    responsejson = response.json()[0]['sha']
+    print ("my response is ", responsejson)
+    appdetail = {
+    "myapplication": [
+        {
+            "version": "1.0",
+            "lastcommitsha": responsejson,
+            "description" : "pre-interview technical test"
+        }
+        ]
+        }
+    return appdetail
+    #return jsonify(response.content)
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+
 
 @app.route('/', methods=['GET'])
 def home():
-    #return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
-    return jsonify(appdetail)
+    #return invokegithubapi()
+    #return jsonify(appdetail)
+    return "Hello, you are on the home page"
 
 
-@app.route('/api/v1/appinfo', methods=['GET'])
+@app.route('/api/v1/version', methods=['GET'])
 def api_all():
-    return jsonify(appdetail)
+    return invokegithubapi()
 
 
 app.run()
