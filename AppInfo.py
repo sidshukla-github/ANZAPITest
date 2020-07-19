a2b02c2f1fa8f81145518fd177b72f2db2a65efc
sidshukla-github <siddharth.shukla@gmail.com>
2020-07-19 22:06:21 +1000

    master

--- AppInfo.py
+++ AppInfo.py
@@ -17,7 +17,8 @@ appdetail = {
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    #return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return jsonify(appdetail)
 
 
 @app.route('/api/v1/appinfo', methods=['GET'])
