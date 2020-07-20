9a3ef2f1cfbde69e22971382ed2cd419b8538328
sidshukla-github <siddharth.shukla@gmail.com>
2020-07-20 23:17:38 +1000

    testing

--- AppUT.py
+++ AppUT.py
@@ -5,8 +5,26 @@ import sys
 import flask 
 from flask import request, jsonify
 
-def test():
-    print ("hellow world")
-    sys.exit(0)
+def startunittest():
+    print ("Starting unit test")
+    
+    urlid = 'https://api.github.com/repos/sidshukla-github/ANZAPITest/commits'
+    tagurlid = "https://api.github.com/repos/sidshukla-github/ANZAPITest/tags"
+    headers = {
+ 
+      "Content-Type": "application/json",
+      "User-Agent" : "sidshukla-github"  
+    }
 
-test()
+    response = requests.request("GET", urlid, headers=headers)
+
+    print ("response is ", response.content)
+
+    if response == 200:
+        sys.exit(0)
+    else:
+        sys.exit(-1)
+
+    
+
+startunittest()
